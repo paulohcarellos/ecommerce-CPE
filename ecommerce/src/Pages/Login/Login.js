79b71958ef5888ae5bbdff75507c9339b5482883
Hélio <helioneto29@hotmail.com>
2020-10-20 16:08:31 -0300

    Login funcional

--- ecommerce/src/Pages/Login/Login.js
+++ ecommerce/src/Pages/Login/Login.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Form, Button} from 'react-bootstrap';
+import {useHistory} from 'react-router-dom';
 import './Login.css';
 
 function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const history = useHistory();
+
+    function login(){
+        if(email === "Hélio" && password === "123"){
+            alert("Bem-vindo! " + email);
+            history.push("home");
+        }else{
+            alert("Email ou senha incorretos");
+        };
+    }
+
     return(
         <pagina>
             <div className="cabecalho">
@@ -12,17 +26,17 @@ function Login() {
             </div>
             <div className="geral">
                 <div className="container">
-                    <h1><b>Entrar</b></h1>
-                    <Form className="inputs">
-                        <Form.Group controlId="email">
-                            <Form.Control type="email" placeholder="Email"/>
-                        </Form.Group>
-                        <Form.Group controlId="senha">
-                            <Form.Control type="password" placeholder="Senha"/>
-                        </Form.Group>
-                        <Button variant="light">Login</Button>{' '}
-                        <Button variant="light">Criar conta</Button>{' '}
-                    </Form>
+                        <h1><b>Entrar</b></h1>
+                        <Form className="inputs">
+                            <Form.Group controlId="email">
+                                <Form.Control type="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}}/>
+                            </Form.Group>
+                            <Form.Group controlId="senha">
+                                <Form.Control type="password" placeholder="Senha" onChange={(e)=>{setPassword(e.target.value)}}/>
+                            </Form.Group>
+                            <Button variant="outline-dark" onClick={login}>Login</Button>
+                            <Button variant="outline-dark" onClick={()=>history.push("cadastro")}>Criar conta</Button>
+                        </Form>
                 </div>
             </div>
             <div className="rodape">
