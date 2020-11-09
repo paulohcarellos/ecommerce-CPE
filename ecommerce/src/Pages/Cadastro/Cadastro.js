ddd57e0e3b515b0d9c75d37fa15af9a003f3fed5
Hélio <helioneto29@hotmail.com>
2020-11-09 14:10:09 -0300

    Revert "Carrinho novo"
    This reverts commit 75f06f8e29ac787a548a454b83c6db4166038469.

--- ecommerce/src/Pages/Cadastro/Cadastro.js
+++ ecommerce/src/Pages/Cadastro/Cadastro.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {Form, Button} from 'react-bootstrap';
 import {useHistory} from 'react-router-dom';
+import {hash} from 'bcryptjs'
 import './Cadastro.css';
 
 function Cadastro() {
