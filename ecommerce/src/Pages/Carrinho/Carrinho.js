2d2e1f2a422467c931913eab758447bda2850bfb
Hélio <helioneto29@hotmail.com>
2020-11-11 19:35:52 -0300

    Carrinho adaptado pro bd

--- ecommerce/src/Pages/Carrinho/Carrinho.js
+++ ecommerce/src/Pages/Carrinho/Carrinho.js
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react';
 import { Nav, Button, Dropdown, DropdownButton} from 'react-bootstrap'
 import { FaShoppingCart } from "react-icons/fa";
 import {useHistory} from 'react-router-dom';
+import Item from "./Item";
+import {fotos} from "./Fotos";
+import Carousel from "react-elastic-carousel";
 import Header from '../../Components/Header'
 import {getUser} from '../../Components/tools'
 import "./Carrinho.css"
@@ -30,75 +33,33 @@ function Carrinho() {
                     </Nav>
                 </Nav>
                 <div id="linha1"/>
-                <div id="Produto">
-                    <div id="Produto1">
-                        <div id="Imagem">
-                            <h3>Playstation 5</h3>
-                            <img src="/imagens/PlayStation5.jpg" alt="Item1" id="Item1"/>
-                            <h4>R$ 4000,00</h4>
-                        </div>
-                        <div id="Atributos">
-                            <Nav id="Vendedor">
-                                Vendedor - Caio
-                            </Nav>
-                            <Nav id="Prazo">
-                                Prazo - 7 dias
-                            </Nav>
-                            <Nav id="Estoque">
-                                Estoque - 3
-                            </Nav>
-                            <Nav id="Compras">
-                                Compras - 172
-                            </Nav>
-                        </div>
-                        <DropdownButton id="dropdown-basic-button" title="Quantidade Itens" variant='Success'>
-                            <Dropdown.Item href="#/action-1">1</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">2</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">3</Dropdown.Item>
-                            <Dropdown.Item href="#/action-1">4</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">5</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">6</Dropdown.Item>
-                        </DropdownButton>
-                        <Nav id="Botão">
-                            <Button variant="outline-info" onClick={()=>history.push("carrinho")}>Remover Item</Button>
-                        </Nav>
-                    </div>
-                </div>
-                <div id="linha"/>
-                <div id="Produto">
-                    <div id="Produto2">
-                        <div id="Imagem">
-                            <h3>Samsung TV</h3>
-                            <img src="/imagens/SamsungSmartTV50CrystalUHD.jpg" alt="Item2" id="Item2"/>
-                            <h4>R$ 3000,00</h4>
-                        </div>
-                        <div id="Atributos">
-                            <Nav id="Vendedor">
-                                Vendedor - João
-                            </Nav>
-                            <Nav id="Prazo">
-                                Prazo - 12 dias
-                            </Nav>
-                            <Nav id="Estoque">
-                                Estoque - 2
-                            </Nav>
-                            <Nav id="Compras">
-                                Compras - 72
-                            </Nav>
-                        </div>
-                        <DropdownButton id="dropdown-basic-button" title="Quantidade Itens" variant="Secondary">
-                            <Dropdown.Item href="#/action-1">1</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">2</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">3</Dropdown.Item>
-                            <Dropdown.Item href="#/action-1">4</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">5</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">6</Dropdown.Item>
-                        </DropdownButton>
-                        <Nav id="Botão">
-                            <Button variant="outline-info" onClick={()=>history.push("carrinho")}>Remover Item</Button>
-                        </Nav>
-                    </div>
-                </div>
+                    {fotos.map( (foto) => {
+                        return(
+                            <div id="componentes">
+                                <div id="atributos">
+                                    <h3>{foto.nome}</h3>
+                                    <img id="img" alt={foto.descricao} src={foto.url}></img>
+                                    <h5>{foto.descricao}</h5>
+                                    <h4>{foto.preco}</h4>
+                                    <h4>Vendedor - {foto.vendedor}</h4>
+                                    <h4>Estoque - {foto.estoque}</h4>
+                                </div> 
+                                <div id="item1">
+                                    <DropdownButton id="dropdown-basic-button" title="Quantidade Itens" variant='Success'>
+                                        <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-1">4</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">5</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">6</Dropdown.Item>
+                                    </DropdownButton>
+                                    <Nav id="Botão">
+                                        <Button variant="outline-info" onClick={()=>history.push("home")}>Remover Item</Button>
+                                    </Nav>
+                                </div>
+                            </div> 
+                        )
+                    })}
                 <div id="linha2"/>
                 <div id="Rodape">
                     <div id="entrega">
---
+++ ecommerce/src/Pages/Carrinho/Fotos.js
@@ -0,0 +1,42 @@
+export const fotos = [
+    {
+        nome: "Playstation 5",
+        url: "/imagens/PlayStation5.jpg",
+        descricao: "Console",
+        preco: "R$4000,00",
+        vendedor: "Caio",
+        estoque: 2
+    },
+    {
+        nome: "Smart Tv",
+        url: "/imagens/SamsungSmartTV50CrystalUHD.jpg",
+        descricao: "Smart TV Samsung",
+        preco: "R$3000,00",
+        vendedor: "João",
+        estoque: 4
+    },
+    {
+        nome: "Air Friyer",
+        url: "/imagens/FritadeiraElétrica.jpg",
+        descricao: "Fritadeira Elétrica",
+        preco: "R$ 250,00",
+        vendedor: "Caio",
+        estoque: 7
+    },
+    {
+        nome: "Moletom de otaku",
+        url: "/imagens/MoletomAkatsukiNaruto.jpg",
+        descricao: "Manto da Akatsuki",
+        preco: "R$ sua alma",
+        vendedor: "Breno",
+        estoque: 1
+    },
+    {
+        nome: "Nintendo Switch",
+        url: "/imagens/NintendoSwitch.jpg",
+        descricao: "Console",
+        preco: "R$ 2600,00",
+        vendedor: "José",
+        estoque: 12
+    },
+]
